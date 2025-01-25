a2a5df763305e4b4e8c44cf89a5d10f4458be16a
fiantyogalihp <fiantiogalih@gmail.com>
2025-01-25 22:14:42 +0700

    feat: add new message utils response

--- utils/error.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-import "github.com/gofiber/fiber/v2"
-
-func SendErrorResponse(c *fiber.Ctx, templates, message string) error {
-	return c.Render(templates, fiber.Map{
-		"error": message,
-	})
-}
