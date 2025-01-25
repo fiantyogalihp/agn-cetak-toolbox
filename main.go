057dc0502ff00d72cd81afb139736300a03d5439
fiantyogalihp <fiantiogalih@gmail.com>
2025-01-25 22:13:52 +0700

    feat: add logger middleware

--- main.go
+++ main.go
@@ -6,7 +6,10 @@ import (
 	"net/http"
 
 	"github.com/fiantyogalihp/dynamic-json-parsing-struct/routers"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/gofiber/template/html/v2"
 )
 
@@ -23,6 +26,14 @@ func main() {
 	app := fiber.New(fiber.Config{
 		Views: engine,
 	})
+
+	// MIDDLEWARE
+	app.Use(recover.New())
+	app.Use(logger.New(logger.Config{
+		Format:     "${time} |${ip}| ${status} - ${method} ${path}\n",
+		TimeFormat: "02 Jan 2006 15:04:05",
+		TimeZone:   "Asia/Jakarta",
+	}))
 
 	// Serve static files (e.g., CSS/JS)
 	app.Static("/static", "./static")
