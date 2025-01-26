4af0b8c7c6707543256952a45ce6193a5341f518
fiantyogalihp <fiantiogalih@gmail.com>
2025-01-26 13:28:34 +0700

    feat: update variable name response

--- utils/message.go
+++ utils/message.go
@@ -5,13 +5,13 @@ import "github.com/gofiber/fiber/v2"
 func SendErrorResponse(c *fiber.Ctx, message string) error {
 	return c.Render("templates/message", fiber.Map{
 		"message": message,
-		"style":   "red",
+		"color":   "red",
 	})
 }
 
 func SendSuccessResponse(c *fiber.Ctx, message string) error {
 	return c.Render("templates/message", fiber.Map{
 		"message": message,
-		"style":   "green",
+		"color":   "green",
 	})
 }
