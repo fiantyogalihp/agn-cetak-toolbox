c5a4065a81b1ef55cf802298ee501974c639b394
fiantyogalihp <fiantiogalih@gmail.com>
2025-01-28 21:22:38 +0700

    feat: update err function response with attribute

--- utils/message.go
+++ utils/message.go
@@ -1,17 +1,21 @@
 package utils
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+)
 
-func SendErrorResponse(c *fiber.Ctx, message string) error {
+func SendErrorResponse(c *fiber.Ctx, componentID, message string) error {
 	return c.Render("templates/message", fiber.Map{
-		"message": message,
-		"color":   "red",
+		"message":      message,
+		"color":        "red",
+		"component_id": componentID,
 	})
 }
 
-func SendSuccessResponse(c *fiber.Ctx, message string) error {
+func SendSuccessResponse(c *fiber.Ctx, componentID, message string) error {
 	return c.Render("templates/message", fiber.Map{
-		"message": message,
-		"color":   "green",
+		"message":      message,
+		"color":        "green",
+		"component_id": componentID,
 	})
 }
