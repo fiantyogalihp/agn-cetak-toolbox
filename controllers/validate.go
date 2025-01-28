a92fcaa9f98453394c296286aa3a966387a7abbd
fiantyogalihp <fiantiogalih@gmail.com>
2025-01-28 21:38:14 +0700

    BREAKING CHANGE: update to a new function name
    changed:
    - handler function name
    - refactor logic and error function
    - add wrap code as function parameter
    - fix wrong spelled variable

--- controllers/validate.go
+++ controllers/validate.go
@@ -3,68 +3,116 @@ package controllers
 import (
 	"embed"
 	"fmt"
-	"log"
 
 	"github.com/fiantyogalihp/dynamic-json-parsing-struct/utils"
 	"github.com/gofiber/fiber/v2"
 )
 
-func ValidateJSONField(c *fiber.Ctx, embedScreen embed.FS) error {
+func ValidateSrcJSONField(c *fiber.Ctx, embedScreen embed.FS) error {
 
 	screenFilename := c.FormValue("screen-choice")
+	jsonInput := c.FormValue("contoh-response")
 
 	// VALIDATE
 	if screenFilename == "" {
-		return utils.SendErrorResponse(c, "Please select your screen!")
+		return utils.SendErrorResponse(c, "response-contoh", "Please select your screen!")
 	}
 
-	screenFilename += ".json"
+	if jsonInput == "" {
+		return utils.SendErrorResponse(c, "response-contoh", "Please input your JSON Field!")
+	}
 
 	// READ SCREEN
-	screeenResult, err := utils.ReadExplicitScreen(embedScreen, screenFilename)
+	screenResult, err := utils.ReadExplicitScreen(embedScreen, screenFilename+".json")
 	if err != nil {
-		return utils.SendErrorResponse(c, err.Error())
+		return utils.SendErrorResponse(c, "response-contoh", err.Error())
 	}
 
 	// GET DATA
-	jsonInput := c.FormValue("contoh-response")
 	errChan := make(chan error, 10)
 
-	// VALIDATE
-	if jsonInput == "" {
-		return utils.SendErrorResponse(c, "Please input your JSON Field!")
+	// GET UNMARSHALED RAW JSON
+	rawJSON, err := utils.UnmarshalDynamicExampleJson(jsonInput)
+	if err != nil {
+		return utils.SendErrorResponse(c, "response-contoh", err.Error())
 	}
-
-	rawJSONChan := make(chan map[string]interface{})
-	go func() {
-		defer close(rawJSONChan)
-
-		result, err := utils.UnmarshalDynamicExampleJson(jsonInput)
-		if err != nil {
-			log.Println(err)
-			errChan <- err
-			return
-		}
-
-		rawJSONChan <- result
-
-	}()
-	rawJSON := <-rawJSONChan
 
 	// CHECK EXIST FIELD
-	for _, field := range screeenResult.Arrange {
+	for _, field := range screenResult.Arrange {
 		if _, ok := rawJSON[field]; !ok {
 			err = fmt.Errorf("invalid field, '%s' is missing", field)
-			return utils.SendErrorResponse(c, err.Error())
+			return utils.SendErrorResponse(c, "response-contoh", err.Error())
 		}
 	}
 
-	utils.CheckExampleJSONInput(errChan, jsonInput, screeenResult.Required)
+	utils.CheckJSONInput(errChan, rawJSON, screenResult.Required, nil)
 
 	// ERROR HANDLE
 	for err := range errChan {
-		return utils.SendErrorResponse(c, err.Error())
+		return utils.SendErrorResponse(c, "response-contoh", err.Error())
 	}
 
-	return utils.SendSuccessResponse(c, "Your JSON Field is Valid!")
+	return utils.SendSuccessResponse(c, "response-contoh", "Your JSON Field is Valid!")
+}
+
+func ValidateDestJSONField(c *fiber.Ctx, embedScreen embed.FS) error {
+	screenFilename := c.FormValue("screen-choice")
+	jsonInput := c.FormValue("update-response")
+
+	// VALIDATE
+	if screenFilename == "" {
+		return utils.SendErrorResponse(c, "response-update", "Please select your screen!")
+	}
+	if jsonInput == "" {
+		return utils.SendErrorResponse(c, "response-update", "Please input your JSON Field!")
+	}
+
+	// READ SCREEN
+	screenResult, err := utils.ReadExplicitScreen(embedScreen, screenFilename+".json")
+	if err != nil {
+		return utils.SendErrorResponse(c, "response-update", err.Error())
+	}
+
+	// GET DATA
+	errChan := make(chan error, 10)
+
+	// GET UNMARSHALED RAW JSON
+	rawJSON, err := utils.UnmarshalDynamicExampleJson(jsonInput)
+	if err != nil {
+		return utils.SendErrorResponse(c, "response-update", err.Error())
+	}
+
+	checkFunc := func(data string) bool {
+		return data == "pay"
+	}
+
+	// CHECK PAY FIELD
+	for key := range rawJSON {
+		if checkFunc(key) {
+			err = fmt.Errorf("cannot process with '%s' json field", key)
+			return utils.SendErrorResponse(c, "response-update", err.Error())
+		}
+	}
+
+	// CHECK EXIST FIELD
+	for _, field := range screenResult.Arrange {
+
+		if checkFunc(field) {
+			continue
+		}
+
+		if _, ok := rawJSON[field]; !ok {
+			err = fmt.Errorf("invalid field, '%s' is missing", field)
+			return utils.SendErrorResponse(c, "response-update", err.Error())
+		}
+	}
+
+	utils.CheckJSONInput(errChan, rawJSON, screenResult.Required, checkFunc)
+
+	// ERROR HANDLE
+	for err := range errChan {
+		return utils.SendErrorResponse(c, "response-update", err.Error())
+	}
+
+	return utils.SendSuccessResponse(c, "response-update", "Your JSON Field is Valid!")
 }
