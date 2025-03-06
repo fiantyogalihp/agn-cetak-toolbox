3fdbbc9823bfcefc77846f20a5c3b0c7af18d1f2
fiantyogalihp <fiantiogalih@gmail.com>
2025-03-06 09:14:56 +0700

    feat: add feature to replace two JSON

--- controllers/print_json.go
+++ controllers/print_json.go
@@ -33,6 +33,8 @@ func PrintJSON(c *fiber.Ctx, embedScreen embed.FS) error {
 	}
 
 	// GET DATA
+	errPrepareChan := make(chan error, 10)
+	errPrintChan := make(chan error, 10)
 	errChan := make(chan error, 10)
 	mu := sync.Mutex{}
 
@@ -41,6 +43,7 @@ func PrintJSON(c *fiber.Ctx, embedScreen embed.FS) error {
 	go func() {
 		defer close(rawExampleJSONChan)
 		defer close(rawUpdateJSONChan)
+		defer close(errChan)
 
 		resultExample, err := utils.UnmarshalDynamicExampleJson(exampleJSONInput)
 		if err != nil {
@@ -65,14 +68,71 @@ func PrintJSON(c *fiber.Ctx, embedScreen embed.FS) error {
 	rawExampleJSON := <-rawExampleJSONChan
 	rawUpdateJSON := <-rawUpdateJSONChan
 
+	for err := range errChan {
+		return utils.SendErrorResponse(c, "response-print", err.Error())
+	}
+
 	rawUpdateJSON["pay"] = rawExampleJSON["pay"]
 
-	jsonData, err := json.Marshal(rawUpdateJSON)
+	// fmt.Println(screenResult)
+
+	fieldKey := make([]string, 0)
+	fieldValue := make([]string, 0)
+	for k, v := range screenResult.Adjustment {
+		fieldKey = append(fieldKey, k)
+		fieldValue = append(fieldValue, v)
+	}
+
+	fmt.Println(fieldKey, fieldValue)
+
+	adjustValResult := make(chan map[string]interface{})
+	utils.PrepareJSONInput(errPrepareChan, rawUpdateJSON, fieldValue, adjustValResult)
+
+	adjustValMap := make(map[string]interface{})
+	for data := range adjustValResult {
+
+		counterKey := 0
+		key := utils.FindKey(counterKey, len(fieldKey), data)
+		if key > len(fieldKey) {
+			errPrepareChan <- fmt.Errorf("key '%d' not found", key)
+			continue
+		}
+
+		keyStr := fmt.Sprint(key)
+
+		adjustValMap[keyStr] = data[keyStr]
+	}
+
+	for err := range errPrepareChan {
+		return utils.SendErrorResponse(c, "response-print", err.Error())
+	}
+
+	resultJSON := make(map[string]interface{})
+	utils.PrintJSONInput(errPrintChan, rawUpdateJSON, fieldKey, adjustValMap, &resultJSON)
+
+	for err := range errPrintChan {
+		return utils.SendErrorResponse(c, "response-print", err.Error())
+	}
+
+	fmt.Println(&resultJSON)
+
+	jsonInqData, err := json.Marshal(resultJSON["inq"])
 	if err != nil {
 		return utils.SendErrorResponse(c, "response-print", err.Error())
 	}
 
-	fmt.Println(screenResult)
+	jsonPayData, err := json.Marshal(resultJSON["pay"])
+	if err != nil {
+		return utils.SendErrorResponse(c, "response-print", err.Error())
+	}
+
+	resultJSON["inq"] = string(jsonInqData)
+	resultJSON["pay"] = string(jsonPayData)
+
+	jsonData, err := json.Marshal(resultJSON)
+	if err != nil {
+		return utils.SendErrorResponse(c, "response-print", err.Error())
+	}
 
 	return c.Render("templates/textarea_result", fiber.Map{
 		"result": string(jsonData),
