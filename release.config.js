6ce11949f717b0959305331637bc283a86072713
fiantyogalihp <fiantiogalih@gmail.com>
2025-03-08 16:29:53 +0700

    chore(deps): add release asset in release page

--- release.config.js
+++ release.config.js
@@ -1,14 +1,17 @@
 const config = {
-  branches: ['main'],
+  branches: ["main"],
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
     // ["@semantic-release/git", {
     //   "assets": ["build/**"],
-    //   "label": "Build Artifacts"
     //   // "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
     // }],
-    '@semantic-release/github'
+    ["@semantic-release/github", {
+      "assets": [
+        { "path": "build/**", "label": "Build Artifacts AGN Cetak Toolbox" },
+      ]
+    }],
   ]
 };
 
